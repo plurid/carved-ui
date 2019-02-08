c72536e103a7c622f3c5bebc970d8f5801b56774
Jan Cavel <caveljan@gmail.com>
2019-02-08 08:48:46 +0200

    dev: CarvedInput

--- src/examples/App.tsx
+++ src/examples/App.tsx
@@ -6,7 +6,36 @@ import { CarvedInput } from "../lib";
 const App = () => (
     <div style={{ width: 640, margin: "15px auto" }}>
         <h1>CarvedInput</h1>
-        <CarvedInput label="Email Address" placeholder="name@example.com" />
+        <br />
+
+        <code>
+            {
+                `
+                <CarvedInput
+                    label="Email Address"
+                    placeholder="name@example.com"
+                />
+                `
+            }
+        </code>
+        <br />
+        <br />
+
+        <CarvedInput
+            label="Name"
+            placeholder="John Smith"
+        />
+        <br />
+        <CarvedInput
+            label="Email Address"
+            placeholder="name@example.com"
+        />
+        <br />
+        <CarvedInput
+            type="password"
+            label="Password"
+        />
+        <br />
     </div>
 );
 
--- src/lib/components/CarvedInput/index.tsx
+++ src/lib/components/CarvedInput/index.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import * as classNames from 'classnames/bind';
 
-import "./styles.scss";
+import styles from './styles.module.scss';
+
+const cx = classNames.bind(styles);
 
 
 
@@ -10,27 +13,78 @@ interface CarvedInputProperties {
     placeholder?: string,
     value?: string,
     onChange?: any,
-    helpText?: string,
+}
+
+interface CarvedInputState {
+    inputFocused: boolean,
+    value: string | undefined,
 }
 
 
-class CarvedInput extends Component<CarvedInputProperties, {}> {
+class CarvedInput extends Component<CarvedInputProperties, CarvedInputState> {
+    constructor(props: CarvedInputProperties) {
+        super(props);
+
+        const { value } = this.props;
+
+        this.state = {
+            inputFocused: false,
+            value: value ? value : ''
+        };
+    }
+
+    onChangeProper = (onChange: any, event: any) => {
+        if (onChange) {
+            onChange(event)
+        }
+
+        this.setState({
+            value: event.target.value,
+        });
+    }
+
+    onFocus = () => {
+        this.setState( {
+            inputFocused: true,
+        });
+    }
+
+    onBlur = () => {
+        this.setState( {
+            inputFocused: false,
+        });
+    }
+
     render() {
-        const { type, label, placeholder, value, onChange, helpText} = this.props;
+        const { type, label, placeholder, onChange} = this.props;
+        const inputType = type ? type : 'text';
+        const { inputFocused } = this.state;
 
         return (
-            <div className="carved-form-group">
-                {label && <label className="carved-text-label">{label}</label>}
+            <div className={ cx(styles.carvedFormGroup) }>
+                <div className={ cx(styles.carvedInputContainer) }>
+                    {
+                        label && (
+                            <label
+                                className={ cx(styles.carvedLabel, {
+                                    [styles.animate]: inputFocused
+                                }) }
+                            >
+                                { this.state.value === '' ? label : ''}
+                            </label>
+                        )
+                    }
 
-                <input
-                    type={type}
-                    className="carved-text-input"
-                    value={value}
-                    onChange={e => onChange && onChange(e.target.value)}
-                    placeholder={placeholder}
-                />
-
-                {helpText && <small className="carved-form-text">{helpText}</small>}
+                    <input
+                        type={inputType}
+                        className={ cx(styles.carvedInput) }
+                        value={this.state.value}
+                        onChange={this.onChangeProper.bind(this, onChange)}
+                        onFocus={this.onFocus}
+                        onBlur={this.onBlur}
+                        placeholder={placeholder}
+                    />
+                </div>
             </div>
         );
     }
