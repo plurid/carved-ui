8c08edfdae0cc51a5113ed5af4db89262cd867a8
Jan Cavel <caveljan@gmail.com>
2019-02-19 12:45:09 +0200

    setup: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import * as React from 'react'
 import { Component } from 'react';
-import { CarvedMenuBar } from 'carved-react';
-import { CarvedFjord } from 'carved-react';
-import { CarvedStratum } from 'carved-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import CarvedTest from './components/pages/CarvedTest';
 
 
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <CarvedStratum>
-                    <div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas adipisci quos deserunt rerum blanditiis saepe quae totam recusandae rem aperiam quo alias fuga eaque, quisquam id cumque maiores laborum dolorum!
-                    </div>
-                </CarvedStratum>
-
-                <CarvedMenuBar>
-                    App
-                </CarvedMenuBar>
-
-                <div style={ { margin: '100px 0' } }>
-                    <CarvedFjord>
-                        <div>
-                            Lorem, ipsum dolor<br /><br /><br /><br /> sit amet consectetur adipisicing elit. Voluptates quis voluptatibus fuga deleniti alias nobis iusto incidunt sint doloremque saepe, molestiae velit ab odit repellat dicta! Inventore minima a asperiores?
-                        </div>
-                    </CarvedFjord>
-
-                    <carved-input type="text" label="Name" placeholder="John" />
-                </div>
-            </div>
+            <Router>
+                <Route path="/test" component={CarvedTest} />
+            </Router>
         );
     }
 }
